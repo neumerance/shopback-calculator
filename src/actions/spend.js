386d3ad06cc4d2e450284e8d2142947359e7f765
jonathan canaveral <neumerance@live.com>
2019-12-01 13:20:27 +0800

    spend calculator module

--- src/actions/spend.js
+++ src/actions/spend.js
@@ -4,7 +4,7 @@ import ExchangeRate from "../services/exchange_rate_service";
 class Spend {
   constructor(domainName, spendings) {
     this.domainName = domainName;
-    this.spendings = spendings;
+    this.spendings = spendings.filter(n => ['number','float'].includes(typeof(n)));
   }
 
   domain() {
@@ -13,7 +13,38 @@ class Spend {
 
   async rate() {
     const data = await new ExchangeRate().getExchangeRate('USD', this.domain().currency);
-    return data.rates[this.domain.currency];
+    return data.rates[this.domain().currency];
+  }
+
+  highestSpending() {
+    return Math.max.apply(null, this.spendings);
+  }
+
+  averageSpending() {
+    if (!this.spendings.length) { return 0; }
+    return this.spendings.reduce((sum, x) => sum + x) / this.spendings.length;
+  }
+
+  cashbackPercentage() {
+    const amount = this.averageSpending();
+    if (amount >= 50) {
+      return .20
+    } else if (amount >= 20) {
+      return .15
+    } else if (amount >= 10) {
+      return .1
+    } else {
+      return .05
+    }
+  }
+
+  async cashback() {
+    let amount = 0;
+    let exchangeRate = await this.rate();
+    if (this.spendings.length && exchangeRate) {
+      amount = this.highestSpending() * this.cashbackPercentage();
+    }
+    return `Award cashback: ${amount / exchangeRate} USD`
   }
 }
 
